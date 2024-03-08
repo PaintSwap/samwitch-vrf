dfcad8dcbd1325e006ddd026e20981bdf8a76c77
Justin Silver <jsilver@doublesharp.com>
2024-03-07 21:40:55 -0800

    update coverage

--- .solcover.js
+++ .solcover.js
@@ -1,14 +1,3 @@
 module.exports = {
-  configureYulOptimizer: true,
-  solcOptimizerDetails: {
-    peephole: false,
-    inliner: false,
-    jumpdestRemover: false,
-    orderLiterals: true, // <-- TRUE! Stack too deep when false
-    deduplicate: false,
-    cse: false,
-    constantOptimizer: false,
-    yul: false,
-  },
   skipFiles: ["test/", "libraries/"],
 };
